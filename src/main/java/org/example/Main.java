7501c5c086d7a27a63cf1032ae0f88f77fcaecef
duvan1026 <duvancastro1026@gmail.com>
2023-03-29 08:36:12 -0500

    implement exercise 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,15 @@ public class Main {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
 
+        //Ejercicio 1
         Saludo saludo = (Saludo) context.getBean("saludo");
         saludo.imprimirSaludo();
+
+        //Ejercicio 2
+        UserService userService = (UserService) context.getBean("userService");
+        userService.notificationService.printSaludo();
+
+
 
 
     }
--- src/main/java/org/example/NotificationService.java
+++ src/main/java/org/example/NotificationService.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NotificationService {
+
+
 
     public void printSaludo(){
         System.out.println("Souy un saludo desde NotificationService");
